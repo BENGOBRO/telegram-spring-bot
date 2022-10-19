811c86faaf5a45d33e3f7f0ed27b69d3225759bd
bengobro <sponkeylox6@gmail.com>
2022-10-19 23:59:06 +0300

    feature:added rabbitmq
    Now messages from user to bot keep in queues (text, docsm photos).

--- dispatcher/src/main/java/ru/bengobro/service/impl/UpdateProducerImpl.java
+++ dispatcher/src/main/java/ru/bengobro/service/impl/UpdateProducerImpl.java
@@ -1,7 +1,8 @@
 package ru.bengobro.service.impl;
 
 import lombok.extern.log4j.Log4j;
-import org.jvnet.hk2.annotations.Service;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.bengobro.service.UpdateProducer;
 
@@ -9,8 +10,16 @@ import ru.bengobro.service.UpdateProducer;
 @Log4j
 public class UpdateProducerImpl implements UpdateProducer {
 
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+
     @Override
     public void produce(String rabbitQueue, Update update) {
         log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
     }
 }
